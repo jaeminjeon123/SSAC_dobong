4246d5a9d883d76b9b63ebb1d2b89c375eb8d8de
jjm <jaeminjeon123@gmail.com>
2024-01-26 10:56:36 +0900

    Update app.js

--- 7.form/app.js
+++ 7.form/app.js
@@ -8,10 +8,11 @@ app.set("views", "./views");
 app.get("/", (req, res) => {
     res.render('index');
 });
-app.get("/getform",function(req,res){
-res.sed('데이터 받았읍니다')
-console.log(req.send)
-)}
+app.get("/getform", function(req, res) {
+    res.send('데이터 받았습니다');
+    console.log(req.query); // 또는 req.params 또는 필요한 요청 데이터
+});
+
 app.listen(port, function() {
     console.log(`Server is running at http://localhost:${port}`);
 });
